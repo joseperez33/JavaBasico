9bf4b89dafda2bcc48671a1e499cd79f57c0a82d
joseperez33 <66077946+joseperez33@users.noreply.github.com>
2020-06-05 17:28:54 -0500

    Actualizacion 1

--- Tarea5/src/Tarea5/ProductoTest.java
+++ Tarea5/src/Tarea5/ProductoTest.java
@@ -3,7 +3,16 @@ package Tarea5;
 public class ProductoTest {
 
 	public static void main(String[] args) {
+
+		for (Productos producto : Productos.values()) {
+
+			System.out.println("Codigo: " + producto.getCodigoProducto());
+			System.out.println("Producto: " + producto);			
+			System.out.println("Valor: " + producto.getValorProducto());
 		
+
+		}
+
 	}
 
 }
--- Tarea5/src/Tarea5/Productos.java
+++ Tarea5/src/Tarea5/Productos.java
@@ -2,7 +2,7 @@ package Tarea5;
 
 public enum Productos {
 
-	PRODUCTO1(1, 100), PRODUCTO2(2, 200), PRODUCTO3(3, 300), PRODUCTO4(4, 400);
+	PRODUCTO_1(1, 1000), PRODUCTO_2(2, 2000), PRODUCTO_3(3, 3000), PRODUCTO_4(4, 4000), PRODUCTO_5(5, 5000), PRODUCTO_6(6, 6000), PRODUCTO_7(7, 7000), PRODUCTO_8(8, 8000), PRODUCTO_9(9, 9000), PRODUCTO_10(10, 10000);
 
 	private final int codigoProducto;
 	private final int valorProducto;
