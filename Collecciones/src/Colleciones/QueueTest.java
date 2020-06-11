2d32f48ef20e5b3d049ff0b233221529c53a079b
joseperez33 <66077946+joseperez33@users.noreply.github.com>
2020-06-11 08:13:36 -0500

    final

--- Collecciones/src/Colleciones/QueueTest.java
+++ Collecciones/src/Colleciones/QueueTest.java
@@ -1,14 +1,21 @@
 package Colleciones;
 
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
+
+/**
+ * Cola FIFO
+ * @author jose.perez
+ *
+ */
 
 public class QueueTest {
 
 	public static void main(String[] args) {
 		Queue<String> Cola1 = new LinkedList<String>();
 		
-		System.out.println("Insertamos 3 elementos en la pila: Juan, Ana y Luis");
+		System.out.println("Insertamos 3 elementos en la Cola: Juan, Ana y Luis");
 		Cola1.add("Hugo");
 		Cola1.add("Paco");
 		Cola1.add("Luis");
@@ -22,6 +29,16 @@ public class QueueTest {
 			System.out.println(Cola1.poll());
 		System.out.println();
 		System.out.println("Cantidad: " + Cola1.size());
+		
+		
+		PriorityQueue<Integer> Cola2 = new PriorityQueue<Integer>();
+		Cola2.add(70);
+		Cola2.add(120);
+		Cola2.add(5);
+		System.out.println("Impresion");
+		while (!Cola2.isEmpty())
+			System.out.println(Cola2.poll());
+		
 
 	}
 
